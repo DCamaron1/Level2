2e0d498dda478969eb9610a8354df0195436125d
ThirdTime <league@WorthyMini.local>
2018-06-30 12:16:29 -0700

    started new branch

--- src/level2Game/GamePanel.java
+++ src/level2Game/GamePanel.java
@@ -11,18 +11,17 @@ import java.awt.event.KeyListener;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class GamePanel extends JPanel implements ActionListener, KeyListener{
+public class GamePanel extends JPanel implements ActionListener, KeyListener {
 	final int MENU_STATE = 0;
 	final int GAME_STATE = 1;
-	final int END_STATE = 2; 
+	final int END_STATE = 2;
 	int currentState = MENU_STATE;
 	Font menuFont;
 	Font endFont;
 	Timer timer;
-	Sadie sadie = new Sadie(250,250,500,50);
+	Sadie sadie = new Sadie(70, 460, 50, 50);
 	ObjectManager manager = new ObjectManager(sadie);
-	
-	
+
 	@Override
 	public void paintComponent(Graphics g) {
 		menuFont = new Font("David", Font.ROMAN_BASELINE, 42);
@@ -34,28 +33,29 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener{
 			drawEndState(g);
 		}
 	}
-	
+
 	public void startGame() {
 		timer.start();
 	}
-	
+
 	public GamePanel() {
 		timer = new Timer(1000 / 60, this);
 	}
-	
+
 	public void updateMenuState() {
-		
+
 	}
-	
+
 	public void updateGameState() {
+		sadie.update();
 		manager.update();
 		manager.manageCloud();
 	}
-	
+
 	public void updateEndState() {
-		
+
 	}
-	
+
 	public void drawMenuState(Graphics g) {
 		g.setColor(Color.lightGray);
 		g.fillRect(0, 0, SadiesDashGame.WIDTH, SadiesDashGame.HEIGHT);
@@ -63,33 +63,31 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener{
 		g.setColor(Color.black);
 		g.drawString("Sadie's Dash", 200, 300);
 	}
-	
+
 	public void drawGameState(Graphics g) {
-		Color skyBlue = new Color(102,204,255);
-		Color groundBrown = new Color(102,51,10);
+		Color skyBlue = new Color(102, 204, 255);
+		Color groundBrown = new Color(102, 51, 10);
 		g.setColor(skyBlue);
 		g.fillRect(0, 0, SadiesDashGame.WIDTH, SadiesDashGame.HEIGHT);
 		g.setColor(groundBrown);
-		g.fillRect(0,500,SadiesDashGame.GROUNDW, SadiesDashGame.GROUNDH);
+		g.fillRect(0, 500, SadiesDashGame.GROUNDW, SadiesDashGame.GROUNDH);
 		manager.draw(g);
-		
-		
-		
+
 	}
-	
+
 	public void drawEndState(Graphics g) {
 		g.setColor(Color.black);
-		g.fillRect(0, 0, SadiesDashGame.WIDTH, SadiesDashGame.HEIGHT);g.setFont(menuFont);
+		g.fillRect(0, 0, SadiesDashGame.WIDTH, SadiesDashGame.HEIGHT);
+		g.setFont(menuFont);
 		g.setColor(Color.BLACK);
 		g.drawString("Created by David Calderon", 200, 300);
-		
+
 	}
-	
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		repaint(); 
-		
+		repaint();
+
 		if (currentState == MENU_STATE) {
 			updateMenuState();
 		} else if (currentState == GAME_STATE) {
@@ -102,12 +100,13 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener{
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		
+
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-			if (e.getKeyCode() == 10) {
+		//System.out.println(e.getKeyCode());
+		if (e.getKeyCode() == 10) {
 			if (currentState == MENU_STATE) {
 				currentState = GAME_STATE;
 			} else if (currentState == GAME_STATE) {
@@ -116,14 +115,18 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener{
 				currentState = MENU_STATE;
 			}
 		}
-		
+
 		if (e.getKeyCode() == 32) {
-			
+			sadie.jumpUp();
+			if(sadie.y <= 400) {
+				sadie.stop();
+				sadie.jumpDown();
+			}
 		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		
+
 	}
 }
--- src/level2Game/Sadie.java
+++ src/level2Game/Sadie.java
@@ -9,14 +9,27 @@ public class Sadie extends GameObject{
 	
 	public Sadie(int x, int y, int width, int height) {
 		super(x, y, width, height);
+		xSpeed = 0;
+		ySpeed = 0;
 	}
 	
 	public void update() {
-		
+		y = y + ySpeed;
 	}
 	
 	public void draw(Graphics g) {
 		g.setColor(Color.red);
-		g.fillRect(50,460,50,50);
+		g.fillRect(x,y,50,50);
+	}
+	public void stop(){
+		ySpeed = 0;
+	}
+	
+	public void jumpUp(){
+		ySpeed = -4;
+	}
+	
+	public void jumpDown(){
+		ySpeed = 4;
 	}
 }
