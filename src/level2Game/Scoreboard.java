fdf7eaa82df8d22a0762e27df0db746b4a57c69e
DCamaron1 <league@Admins-Mac-mini.local>
2018-09-08 12:12:46 -0700

    9-8-18
    Started branch

---
+++ src/level2Game/Scoreboard.java
@@ -0,0 +1,26 @@
+package level2Game;
+
+import java.awt.Font;
+import java.awt.Graphics;
+
+public class Scoreboard {
+	long score = 0;
+	Font font;
+	
+	public void paintComponent(Graphics g) {
+		font = new Font("David", Font.ROMAN_BASELINE, 42);
+	}
+		
+	public Scoreboard() {
+		
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void draw(Graphics g) {
+		
+	}
+	
+}
